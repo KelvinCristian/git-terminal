2fec52d588feb2ec267644c24bb054523792e59c
Kelvin Cristian <christiangamesoficial@gmail.com>
2022-07-19 01:35:24 -0300

    barbosaA-kelvin-cristian

---
+++ projeto intro-web/script.js
@@ -0,0 +1,51 @@
+// comentario de linha 
+/* comentario de 
+bloco */
+
+ /*  const nomePessoa = "kelvin cristian alves baptista"
+ console.log(nomePessoa)
+
+ let idadePessoa = 22
+ idadePessoa = 22.5
+ idadePessoa = 23
+ console.log("O nome é: ", nomePessoa, "E idade: ", idadePessoa)*/
+
+ /*let temPet = true // false
+ 
+ console.log(temPet)
+ 
+ let nomePet = "preto" //= null //= "preto"
+ console.log(nomePet)
+
+ console.log(typeof nomePet)
+ console.log(typeof idadePet)*/
+
+ 
+/*let idade = prompt("qual sua idade")
+const nome = prompt("qual e seu nome")
+console.log(nome, idade)
+const idadeNumber = Number(idade)
+const idadeString = idade.toString()
+
+console.log("nome",typeof nome)
+console.log("idade",typeof idade)
+console.log(typeof idadeNumber)
+console.log(typeof idadeString)*/
+ 
+
+/*let dia = 18
+const mes = "julho"
+let e2022 = true
+
+console.log(typeof dia)
+console.log(typeof mes)
+console.log(typeof e2022)
+
+let a =20
+let b =30
+let c
+c=a
+a=b
+b=c
+
+console.log( 'A ',a,'b ', b, )*/
